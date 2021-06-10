42a7f5b07d9e25cc041a4faa695720990f9492b5
Pierre Molinaro <pierre@pcmolinaro.name>
2021-06-10 11:10:14 +0200

    Version 1.0.1: doc update

--- src/call-pioasm.py
+++ src/call-pioasm.py
@@ -6,18 +6,40 @@ import sys, os, subprocess
 
 #---------------------------------------------------------------------------------------------------
 
+def RED () :
+  return '\033[91m'
+
+#---------------------------------------------------------------------------------------------------
+
+def ENDC () :
+  return '\033[0m'
+
+#---------------------------------------------------------------------------------------------------
+
+def BOLD () :
+  return '\033[1m'
+
+#---------------------------------------------------------------------------------------------------
+
 #--- Get script absolute path
 scriptDir = os.path.dirname (os.path.abspath (sys.argv [0]))
 os.chdir (scriptDir)
 #---
-pioasmTool = os.path.expanduser ("~/Library/Arduino15/packages/rp2040/tools/pqt-pioasm/1.1.0-a-81a1771/pioasm")
-#--- charlieplexing5
-returncode = subprocess.call ([pioasmTool, "charlieplexing5.pioasm", "pio-charlieplexing5.h"])
-if returncode != 0 :
-  sys.exit (returncode)
-#--- charlieplexing6
-returncode = subprocess.call ([pioasmTool, "charlieplexing6.pioasm", "pio-charlieplexing6.h"])
-if returncode != 0 :
-  sys.exit (returncode)
+toolDir = os.path.expanduser ("~/Library/Arduino15/packages/rp2040/tools/pqt-pioasm")
+found = False
+for aDir in sorted (os.listdir (toolDir)) :
+  pioasmTool = (toolDir + "/" + aDir + "/pioasm")
+  if os.path.exists (pioasmTool) :
+    found = True
+    #--- charlieplexing5
+    returncode = subprocess.call ([pioasmTool, "charlieplexing5.pioasm", "pio-charlieplexing5.h"])
+    if returncode != 0 :
+      sys.exit (returncode)
+    #--- charlieplexing6
+    returncode = subprocess.call ([pioasmTool, "charlieplexing6.pioasm", "pio-charlieplexing6.h"])
+    if returncode != 0 :
+      sys.exit (returncode)
+if not found :
+  print (RED () + BOLD () + "Cannot find pioasm" + ENDC ())
 
 #---------------------------------------------------------------------------------------------------
